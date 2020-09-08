f40b444092a5340b9e48c4ed423a6ad88716a30d
Esrod Zuch <xjysb@qq.com>
2020-09-08 18:17:38 +0800

    prepared for calibration

--- simulink/run_all_points.m
+++ simulink/run_all_points.m
@@ -4,7 +4,7 @@ clear;
 warning( 'off' );
 
 %%  ----- Set Parameters -----
-addr_td1 = [ 'ZSB_A.csv' ];
+addr_td1 = [ 'ZSB_A1.csv' ];
 sim_time = 2;
 collect = 0.5;
 
@@ -15,10 +15,11 @@ for  i = 1 : n
     [W_average,dPhi_average,W_pr,t_vec,y_vec] = simulinkrun( u(i,:), sim_time, collect );
     W_average1(i,:) = W_average;
     dPhi_average1(i,1:2) = dPhi_average;
+    m(i,:)=dPhi_average1(i,1);
     dPhi_average1(i,3) = dPhi_average(1) / dPhi_average(2);
     W_pr1(i,:) = W_pr;
-    figure(i);
-    plot(t_vec,y_vec);
-    grid on;
-    hold on;
+    %figure(i);
+    %plot(t_vec,y_vec);
+    %grid on;
+    %hold on;
 end
